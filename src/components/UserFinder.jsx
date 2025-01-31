5ae9c15995aa27f6369c8b8944c917b6a6a706de
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-30 22:16:52 -0600

    componentDidUpdate

--- src/components/UserFinder.jsx
+++ src/components/UserFinder.jsx
@@ -18,7 +18,15 @@ export default class UserFinder extends Component {
       searchTerm: "DUMMY_USERS",
     };
   }
-
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: DUMMY_USERS.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
   searchChangeHandler(event) {
     this.setState({ searchTerm: event.target.value });
   }
@@ -28,7 +36,7 @@ export default class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={filteredUsers} />
+        <Users users={this.state.filteredUsers} />
       </Fragment>
     );
   }
