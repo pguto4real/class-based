145adce431cfe8efa11c3586f075fb988244d9b1
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-30 21:38:07 -0600

    first example

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,23 @@
-import classes from './User.module.css';
+// import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
-export default User;
+// export default User;
+
+
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class User extends Component {
+
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
