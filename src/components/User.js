29a1dad494fc7eee7886eaa0bb172a3ac1e3cbdf
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-30 21:43:49 -0600

    class based 2

--- src/components/User.js
+++ src/components/User.js
@@ -1,4 +1,4 @@
-// import classes from './User.module.css';
+import classes from "./User.module.css";
 
 // const User = (props) => {
 //   return <li className={classes.user}>{props.name}</li>;
@@ -6,18 +6,11 @@
 
 // export default User;
 
-
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+;
 
 export default class User extends Component {
-
-
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    return <li className={classes.user}>{this.props.name}</li>;
   }
 }
